ff89e27b3e556334f183854bae5ca9093fe706db
Mitch Gillin <mitchtgillin@gmail.com>
2021-06-16 08:39:09 -0400

    fix package #

--- src/utils/suggestions.js
+++ src/utils/suggestions.js
@@ -1,15 +1,16 @@
 // flattens a nested array
 const flatten = arr =>
 	arr.reduce(
-		(flat, toFlatten) => flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten),
-		[],
+		(flat, toFlatten) =>
+			flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten),
+		[]
 	);
 
-const extractSuggestion = (val) => {
+const extractSuggestion = val => {
 	switch (typeof val) {
-		case 'string':
+		case "string":
 			return val;
-		case 'object':
+		case "object":
 			if (Array.isArray(val)) {
 				return flatten(val);
 			}
@@ -24,16 +25,38 @@ function replaceDiacritics(s) {
 	let str = s ? String(s) : "";
 
 	const diacritics = [
-		/[\300-\306]/g, /[\340-\346]/g, // A, a
-		/[\310-\313]/g, /[\350-\353]/g, // E, e
-		/[\314-\317]/g, /[\354-\357]/g, // I, i
-		/[\322-\330]/g, /[\362-\370]/g, // O, o
-		/[\331-\334]/g, /[\371-\374]/g, // U, u
-		/[\321]/g, /[\361]/g, // N, n
-		/[\307]/g, /[\347]/g, // C, c
+		/[\300-\306]/g,
+		/[\340-\346]/g, // A, a
+		/[\310-\313]/g,
+		/[\350-\353]/g, // E, e
+		/[\314-\317]/g,
+		/[\354-\357]/g, // I, i
+		/[\322-\330]/g,
+		/[\362-\370]/g, // O, o
+		/[\331-\334]/g,
+		/[\371-\374]/g, // U, u
+		/[\321]/g,
+		/[\361]/g, // N, n
+		/[\307]/g,
+		/[\347]/g // C, c
 	];
 
-	const chars = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'N', 'n', 'C', 'c'];
+	const chars = [
+		"A",
+		"a",
+		"E",
+		"e",
+		"I",
+		"i",
+		"O",
+		"o",
+		"U",
+		"u",
+		"N",
+		"n",
+		"C",
+		"c"
+	];
 
 	for (let i = 0; i < diacritics.length; i += 1) {
 		str = str.replace(diacritics[i], chars[i]);
@@ -47,7 +70,7 @@ const getSuggestions = ({
 	suggestions,
 	currentValue,
 	suggestionProperties = [],
-	showDistinctSuggestions = false,
+	showDistinctSuggestions = false
 }) => {
 	/*
 		fields: DataFields passed on Search Components
@@ -64,27 +87,33 @@ const getSuggestions = ({
 	const populateSuggestionsList = (val, parsedSource, source) => {
 		// check if the suggestion includes the current value
 		// and not already included in other suggestions
-		const isWordMatch = skipWordMatch || currentValue
-			.trim()
-			.split(' ')
-			.some(term =>
-				replaceDiacritics(val)
-					.toLowerCase()
-					.includes(replaceDiacritics(term)));
+		const isWordMatch =
+			skipWordMatch ||
+			currentValue
+				.trim()
+				.split(" ")
+				.some(term =>
+					replaceDiacritics(val)
+						.toLowerCase()
+						.includes(replaceDiacritics(term))
+				);
 		// promoted results should always include in suggestions even there is no match
 		if ((isWordMatch && !labelsList.includes(val)) || source._promoted) {
 			const defaultOption = {
 				label: val,
 				value: val,
-				source,
+				source
 			};
 			let additionalKeys = {};
-			if (Array.isArray(suggestionProperties) && suggestionProperties.length > 0) {
-				suggestionProperties.forEach((prop) => {
+			if (
+				Array.isArray(suggestionProperties) &&
+				suggestionProperties.length > 0
+			) {
+				suggestionProperties.forEach(prop => {
 					if (parsedSource.hasOwnProperty(prop)) {
 						additionalKeys = {
 							...additionalKeys,
-							[prop]: parsedSource[prop],
+							[prop]: parsedSource[prop]
 						};
 					}
 				});
@@ -92,7 +121,7 @@ const getSuggestions = ({
 
 			const option = {
 				...defaultOption,
-				...additionalKeys,
+				...additionalKeys
 			};
 			labelsList = [...labelsList, val];
 			suggestionsList = [...suggestionsList, option];
@@ -105,16 +134,16 @@ const getSuggestions = ({
 		return false;
 	};
 
-	const parseField = (parsedSource, field = '', source = parsedSource) => {
-		if (typeof parsedSource === 'object') {
-			const fieldNodes = field.split('.');
+	const parseField = (parsedSource, field = "", source = parsedSource) => {
+		if (typeof parsedSource === "object") {
+			const fieldNodes = field.split(".");
 			const label = parsedSource[fieldNodes[0]];
 			if (label) {
 				if (fieldNodes.length > 1) {
 					// nested fields of the 'foo.bar.zoo' variety
 					const children = field.substring(fieldNodes[0].length + 1);
 					if (Array.isArray(label)) {
-						label.forEach((arrayItem) => {
+						label.forEach(arrayItem => {
 							parseField(arrayItem, children, source);
 						});
 					} else {
@@ -126,10 +155,12 @@ const getSuggestions = ({
 						if (Array.isArray(val)) {
 							if (showDistinctSuggestions) {
 								return val.some(suggestion =>
-									populateSuggestionsList(suggestion, parsedSource, source));
+									populateSuggestionsList(suggestion, parsedSource, source)
+								);
 							}
 							val.forEach(suggestion =>
-								populateSuggestionsList(suggestion, parsedSource, source));
+								populateSuggestionsList(suggestion, parsedSource, source)
+							);
 						}
 						return populateSuggestionsList(val, parsedSource, source);
 					}
@@ -141,12 +172,12 @@ const getSuggestions = ({
 
 	const traverseSuggestions = () => {
 		if (showDistinctSuggestions) {
-			suggestions.forEach((item) => {
+			suggestions.forEach(item => {
 				fields.some(field => parseField(item, field));
 			});
 		} else {
-			suggestions.forEach((item) => {
-				fields.forEach((field) => {
+			suggestions.forEach(item => {
+				fields.forEach(field => {
 					parseField(item, field);
 				});
 			});
